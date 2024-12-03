256fb74ddebcaff1c34616bdc5091775993a1bc4
elliotabrahamsson <elliotsson@outlook.com>
2024-12-03 19:55:14 +0100

    fixed the upload if image so it goes to the olympia folder

--- server.js
+++ server.js
@@ -1,54 +1,44 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
-const fs = require("fs");
+const multer = require("multer");
 const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
+
+const uploadFolder = path.join(__dirname, "Olympia Images");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadFolder);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+server.use(cors({ origin: "https://elliotabrahamsson.github.io" }));
 
 server.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
 
-server.use(
-  cors({
-    origin: "https://elliotabrahamsson.github.io",
-  })
-);
 server.use(middlewares);
+server.use(router);
 
-server.post("/upload", (req, res) => {
-  const { imageData, imageName } = req.body;
-
-  if (!imageData || !imageName) {
-    return res.status(400).json({ error: "Invalid request" });
+server.post("/uploadImage", upload.single("picture"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("Ingen fil uppladdad.");
   }
 
-  const base64Data = imageData.replace(
-    /^data:image\/(png|jpg|jpeg);base64,/,
-    ""
-  );
-  const imagePath = path.join(__dirname, "public", `${imageName}.png`);
-
-  if (fs.existsSync(path.join(_dirname, "uploads"))) {
-    fs.mkdirSync(path.join(__dirname, "uploads"));
-  }
-
-  fs.writeFile(imagePath, base64Data, "base64", (err) => {
-    if (err) {
-      console.error(err);
-      return res
-        .status(500)
-        .json({ error: "Error saving image", details: err });
-    }
-
-    const imageURL = `http://localhost:3000/${imageName}.png`;
-    res.status(200).json({ message: "bild uppladdad", imageURL });
-  });
+  const imageUrl = `/Olympia Images/${req.file.filename}`;
+  res.json({ imageUrl });
 });
 
-server.use(router);
+server.use("/Olympia Images", jsonServer.static(uploadFolder));
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
